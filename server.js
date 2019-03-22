ace1047d396fa17998f782583ad2af664d2c5cbc
Syed Adiba Shuhin <adibashuhin@gmail.com>
2019-03-22 17:28:05 +0600

    For Production Build

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const Express = require('express');
 
 const BodyParser = require('body-parser');
 
-const items = require('./routes/api/items');
+const Path = require(path);
 
+const items = require('./routes/api/items');
 
 const app = Express();
 
@@ -18,7 +19,15 @@ Mongoose.connect("mongodb://localhost/list", (err, db) => {
   console.log('Connected');
 });
 
-app.use('/api/items', items )
+app.use('/api/items', items)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
 
 const port = process.env.PORT || 5000;
-app.listen(port,() => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`))
